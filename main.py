6679524a22f1859145fb6010e5b8b917131433e5
sent1nu11 <sent1nu11@gmail.com>
2021-09-07 22:59:49 -0600

    2

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
 import pandas
 data = pandas.read_csv("weather_data.csv")
+# print(type(data))
+# print(data["temp"])
 
-print(data["temp"])
+data_dict = data.to_dict()
+print(data_dict)
+
+temp_list = data["temp"].to_list()
+print(len(temp_list))
+
+print(data["temp"].mean())
